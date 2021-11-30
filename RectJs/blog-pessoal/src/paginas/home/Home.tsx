ea9a5a2fbd754a008811fcbb8b89a234f979c720
Andherline <sciosubitis@gmail.com>
2021-11-30 16:21:31 -0300

    Primeira aplicação em Rect

---
+++ RectJs/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './home.css';
+//t
+function Home()
+{
+    return(
+        
+        <>
+            <h1 className='titulo'>Teste</h1>
+            <img src='https://i.imgur.com/CBL3NU0.jpeg' alt='imagem Tela Principal' className='img'/>
+        </>
+        
+    );
+}
+
+export default Home;

1fc41bf8814646f91d84a2de8ac97ae98e1eaf2a
Andherline <sciosubitis@gmail.com>
2021-12-01 23:38:20 -0300

    Criação da Home

--- RectJs/blog-pessoal/src/paginas/home/Home.tsx
+++ RectJs/blog-pessoal/src/paginas/home/Home.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import './home.css';
-import {Button, Box, Paper} from '@material-ui/core';
+import { Grid, Box, Typography, Button } from '@material-ui/core'
 
-//Box p={2}: p é de padding(espaçamento interno dos elementos)
-function Home()
-{
-    return(
+
+function Home() {
+    return (
         <>
-            <h1>Home</h1>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#2a3948" }}>
+                <Grid item alignItems="center" xs={6}>
+                    <Box paddingX={20}>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>
+                            Seja bem vinda(o)!
+                        </Typography >
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>
+                            Se expresse aqui!
+                        </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center" >
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#86a0bb", color: "white"}}>
+                            Ver Postagens
+                        </Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src="https://images.unsplash.com/photo-1527679124583-9208be990bb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80" alt="Image Teste" width="500px" height="500px"/>
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white"}}>
+                </Grid>
+            </Grid>
         </>
-        
     );
 }
 
